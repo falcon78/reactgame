175ee8d6e1fedad79ed0e8e32cd5bc7b4b5b3188
Falcon Hawk <18nc021@ms.dendai.ac.jp>
2019-03-10 23:51:43 +0900

    Updates

---
+++ precache-manifest.b371b88573ca4428b8f70620077f2880.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1055c74b983db28e4caf",
+    "url": "/reactgame/static/js/runtime~main.c8050e42.js"
+  },
+  {
+    "revision": "5dd93792b63a994aabf0",
+    "url": "/reactgame/static/js/main.30c85aa3.chunk.js"
+  },
+  {
+    "revision": "036f1fd0e44e3723d5f1",
+    "url": "/reactgame/static/js/2.ecf5305d.chunk.js"
+  },
+  {
+    "revision": "5dd93792b63a994aabf0",
+    "url": "/reactgame/static/css/main.766f5202.chunk.css"
+  },
+  {
+    "revision": "21d15c4a24c91ede8c858656464645ec",
+    "url": "/reactgame/index.html"
+  }
+];
